07671b2fb414b66bb1b5cdb8f1b0d1672cab01f0
Charles Goddard <chargoddard@gmail.com>
2025-04-03 20:02:07 -0700

    tweak

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -909,22 +909,21 @@ def get_logprobs(
         # We need to compute top k only if there exists logprobs > 0.
         if largest_num_logprobs > 0:
             if sampling_params.logprob_multinomial_sampling:
-                probs = torch.exp(logprobs)
-                # (num_query_tokens_across_batch, num_vocab).
-                sampled_indices = _multinomial(
-                    probs[query_indices_gpu], largest_num_logprobs)
-                # (num_query_tokens_across_batch, num_logprobs).
-                top_logprobs = logprobs[query_indices_gpu,
-                                       sampled_indices].to('cpu')
-                top_token_ids = sampled_indices.to('cpu')
+                probs = logprobs.exp()  # (N x vocab_size)
+                # Sample "largest_num_logprobs" token indices from
+                # the probability distribution of each row.
+                top_token_ids = torch.multinomial(
+                    probs, largest_num_logprobs,
+                    replacement=False)  # (N, largest_num_logprobs)
+                top_logprobs = torch.gather(logprobs, 1, top_token_ids)
             else:
                 # Logprobs of topk tokens for a batch of sequence groups.
                 # (num_query_tokens_across_batch).
                 top_logprobs, top_token_ids = torch.topk(logprobs,
-                                                        largest_num_logprobs,
-                                                        dim=-1)
-                top_logprobs = top_logprobs.to('cpu')
-                top_token_ids = top_token_ids.to('cpu')
+                                                         largest_num_logprobs,
+                                                         dim=-1)
+            top_logprobs = top_logprobs.to('cpu')
+            top_token_ids = top_token_ids.to('cpu')
 
         selected_logprobs = selected_logprobs.to('cpu')
         ranks = ranks.to('cpu')
